875bd99b06f74eb4ed65d9f8deb98564fbe70521
Jackson Ming Hu <huming2207@gmail.com>
2022-03-13 20:01:36 +1100

    Fix test

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,33 +1,33 @@
-import { readFile, writeFile } from 'fs/promises'
+import { promises as fs } from 'fs'
 
 import test from 'ava'
 
 import { to4Bpp, toMonochrome } from '../index'
 
 test('Read test1 and convert to 4bpp', async (t) => {
-  const input = await readFile('./__test__/test1.png')
+  const input = await fs.readFile('./__test__/test1.png')
   const output = await to4Bpp(input)
   t.true(output !== undefined)
-  await writeFile('./__test__/test1-4bpp.png', output)
+  await fs.writeFile('./__test__/test1-4bpp.png', output)
 })
 
 test('Read test1 and convert to monochrome', async (t) => {
-  const input = await readFile('./__test__/test1.png')
+  const input = await fs.readFile('./__test__/test1.png')
   const output = await toMonochrome(input)
   t.true(output !== undefined)
-  await writeFile('./__test__/test1-mono.png', output)
+  await fs.writeFile('./__test__/test1-mono.png', output)
 })
 
 test('Read chickenfeet and convert to 4bpp', async (t) => {
-  const input = await readFile('./__test__/chickenfeet.jpg')
+  const input = await fs.readFile('./__test__/chickenfeet.jpg')
   const output = await to4Bpp(input)
   t.true(output !== undefined)
-  await writeFile('./__test__/chickenfeet-4bpp.png', output)
+  await fs.writeFile('./__test__/chickenfeet-4bpp.png', output)
 })
 
 test('Read chickenfeet and convert to monochrome', async (t) => {
-  const input = await readFile('./__test__/chickenfeet.jpg')
+  const input = await fs.readFile('./__test__/chickenfeet.jpg')
   const output = await toMonochrome(input)
   t.true(output !== undefined)
-  await writeFile('./__test__/chickenfeet-mono.png', output)
+  await fs.writeFile('./__test__/chickenfeet-mono.png', output)
 })
